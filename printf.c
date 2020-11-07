5697ab6d6e82b624b969ed89224d19f07cdd03ea
bencouser <73387835+bencouser@users.noreply.github.com>
2020-11-07 13:46:03 +0000

    Create printf.c
    learning how to properly use printf with variables.

---
+++ printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+        int favNum = 90;
+        printf("My favourite %s are %d and %f \n", "numbers", favNum, 0.420);
+
+        return 0;
+
+}
