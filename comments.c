0666aeb6a76038f6e4af23babfddc6539769cb7e
bencouser <73387835+bencouser@users.noreply.github.com>
2020-11-07 14:01:21 +0000

    Create comments.c

---
+++ comments.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+        /* 
+
+        comment comment comment
+        
+        */
+        printf("Comments are good \n");
+
+        /*
+        printf("I am self aware \n");
+        */
+
+        return 0;
+}
