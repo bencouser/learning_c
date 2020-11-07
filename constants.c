c1f36355c99875e6448b5ae55002615e82151141
bencouser <73387835+bencouser@users.noreply.github.com>
2020-11-07 14:06:54 +0000

    Create constants.c

---
+++ constants.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+        const int NUM = 5;
+        printf("%d \n", NUM);
+
+        /*
+        NUM = 8;        gives error
+        */
+        printf("%d \n", NUM);
+
+        return 0;
+}
