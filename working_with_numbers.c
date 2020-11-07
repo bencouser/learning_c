b291f09788857fbe468e39becb33c1a293db5378
bencouser <73387835+bencouser@users.noreply.github.com>
2020-11-07 13:56:20 +0000

    Create working_with_numbers.c
    basic maths

---
+++ working_with_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+
+        int num = 5;
+        printf("%d \n", num);
+        printf("%d \n", 5 / 4);
+        printf("%f \n", 5 + 4.0);
+
+        printf("%f \n", pow(2, 3));
+        printf("%f \n", sqrt(4));
+        printf("%f \n", ceil(77.7));
+        printf("%f \n", floor(36.574));
+
+        return 0;
+
+}
