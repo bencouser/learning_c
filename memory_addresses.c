94e9df2372bd1e4798b03b30e18fc3b89d300d45
bencouser <73387835+bencouser@users.noreply.github.com>
2020-11-16 17:46:16 +0000

    Update memory_addresses.c
    pointers

--- memory_addresses.c
+++ memory_addresses.c
@@ -4,8 +4,11 @@
 int main(){
 
   int age = 30;
+  int * pAge = &age;
   double gpa = 3.4;
+  double * pGpa = &gpa;
   char grade = 'A';
+  char * pGrade = &grade;
 
   printf("age: %p\ngpa: %p\ngrade: %p\n", &age, &gpa, &grade);
 
